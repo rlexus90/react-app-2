3303e6e8d86795ddd571f754ad539374246f275e
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2023-11-11 15:55:57 +0200

    refractor: 'NotFound'

--- src/ViewComponent/NotFound/NotFound.tsx
+++ src/ViewComponent/NotFound/NotFound.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './NotFound.module.scss';
-console.log(document.baseURI);
+
 export default function NotFound() {
+  const navigate = useNavigate();
   return (
-    <div className={styles.notFound}>
+    <div
+      className={styles.notFound}
+      onClick={() => {
+        navigate('/');
+      }}
+    >
       <h1>Sorry... Page not found</h1>
     </div>
   );
