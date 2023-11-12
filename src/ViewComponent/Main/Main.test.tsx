ffa6f36be9b4d21d1dddc18960b21fc78ba8a731
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2023-11-12 22:15:08 +0200

    feat: test paggination component

--- src/ViewComponent/Main/Main.test.tsx
+++ src/ViewComponent/Main/Main.test.tsx
@@ -1,6 +1,6 @@
 // import { describe, it, expect } from 'vitest';
-// import { createRoot } from "react-dom/client";
-// import respMock from '../../common/Mocks/respMock'
+// // import { createRoot } from "react-dom/client";
+// // import respMock from '../../common/Mocks/respMock'
 
 // it('test',()=>{
 //   expect(true).toBe(true)
