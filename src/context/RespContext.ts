77298d3fa0d624470dc1b9029a923bf967cb3c21
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2023-11-12 13:07:33 +0200

    refractor: del unusing code

--- src/context/RespContext.ts
+++ src/context/RespContext.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 import { RespContextValue } from '../types/types';
 
 export const RespContext = createContext<RespContextValue>({
-  respParam: { page: '1', limit: '10' },
+  respParam: {},
   setRespParam: () => null,
   filmResp: undefined,
   setFilmResp: () => null,
