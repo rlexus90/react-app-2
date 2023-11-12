8639edc3c96a080e4a7b23cc3e9adef08bdec768
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2023-11-12 22:27:03 +0200

    test huskt

--- src/ViewComponent/Main/Main.test.tsx
+++ src/ViewComponent/Main/Main.test.tsx
@@ -1,7 +1,7 @@
-// import { describe, it, expect } from 'vitest';
-// // import { createRoot } from "react-dom/client";
-// // import respMock from '../../common/Mocks/respMock'
+import { describe, it, expect } from 'vitest';
+// import { createRoot } from "react-dom/client";
+// import respMock from '../../common/Mocks/respMock'
 
-// it('test',()=>{
-//   expect(true).toBe(true)
-// })
+it('test',()=>{
+  expect(true).toBe(true)
+})
