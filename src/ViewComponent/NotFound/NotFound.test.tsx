172bd09168a6f740a91ef034f371caa14677d682
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2023-11-14 16:03:23 +0200

    refractor: update test

--- src/ViewComponent/NotFound/NotFound.test.tsx
+++ src/ViewComponent/NotFound/NotFound.test.tsx
@@ -1,15 +1,20 @@
-import { it, expect } from 'vitest';
-
+import { it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-
 import '@testing-library/jest-dom';
-
 import APIResponce from '../../controller/APIResponse';
-
 import MoreInfo from '../Modal/MoreInfo';
 import App from '../../App';
 import NotFound from './NotFound';
+import { respMock } from '../../common/Mocks/respMock';
+import { RequestAns } from '../../types/types';
+
+vi.mock('./utils/utils', () => ({
+  queryToAPI: vi.fn().mockResolvedValue(respMock),
+}));
+const resp = { type: 'return', value: { results: null } } as unknown as RequestAns;
+const getmovie = vi.spyOn(APIResponce, 'getMovie');
+getmovie.mockResolvedValue(resp)
 
 it('Test not found page', async () => {
   const rote = createMemoryRouter(
@@ -44,4 +49,5 @@ it('Test not found page', async () => {
 
   const text = await screen.findByText('Sorry... Page not found');
   expect(text).toBeInTheDocument();
+	console.log(getmovie.mock.results)
 });
